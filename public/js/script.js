a94070042e3929de73b852ebd7b7fccbbf9a4098
GoRFy <lambot.rom@gmail.com>
2016-10-26 10:39:18 +0200

    Code du prof + mes modifs

---
+++ public/js/script.js
@@ -0,0 +1,123 @@
+$(document).ready(function(){
+
+  var scope = ["public_profile","email"];
+
+$('#facebookLogin').click(function(){
+  FB.login(function(response) {
+    statusChangeCallback(response);
+  },{scope:scope.join()});
+})
+
+$('#facebookLogout').click(function(){
+  FB.logout(function(response) {
+    statusChangeCallback(response);
+  });
+})
+ function statusChangeCallback(response) {
+   console.log('statusChangeCallback');
+   console.log(response);
+   // The response object is returned with a status field that lets the
+   // app know the current login status of the person.
+   // Full docs on the response object can be found in the documentation
+   // for FB.getLoginStatus().
+   if (response.status === 'connected') {
+     $('#facebookLogin').hide();
+     $('#facebookLogout').show();
+     // Logged into your app and Facebook.
+     if(verifyScope()){
+        testAPI();
+     }
+   } else if (response.status === 'not_authorized') {
+     // The person is logged into Facebook, but not your app.
+     document.getElementById('status').innerHTML = 'Please log ' +
+       'into this app.';
+       $('#facebookLogin').show();
+       $('#facebookLogout').hide();
+   } else {
+     // The person is not logged into Facebook, so we're not sure if
+     // they are logged into this app or not.
+     document.getElementById('status').innerHTML = 'Please log ' +
+       'into Facebook.';
+       $('#facebookLogin').show();
+       $('#facebookLogout').hide();
+   }
+ }
+
+ // This function is called when someone finishes with the Login
+ // Button.  See the onlogin handler attached to it in the sample
+ // code below.
+ function checkLoginState() {
+   FB.getLoginStatus(function(response) {
+     statusChangeCallback(response);
+   });
+ }
+
+ window.fbAsyncInit = function() {
+ FB.init({
+   appId      : '1462037537430458',
+   cookie     : true,  // enable cookies to allow the server to access
+                       // the session
+   xfbml      : true,  // parse social plugins on this page
+   version    : 'v2.5' // use graph api version 2.5
+ });
+
+ // Now that we've initialized the JavaScript SDK, we call
+ // FB.getLoginStatus().  This function gets the state of the
+ // person visiting this page and can return one of three states to
+ // the callback you provide.  They can be:
+ //
+ // 1. Logged into your app ('connected')
+ // 2. Logged into Facebook, but not your app ('not_authorized')
+ // 3. Not logged into Facebook and can't tell if they are logged into
+ //    your app or not.
+ //
+ // These three cases are handled in the callback function.
+
+ FB.getLoginStatus(function(response) {
+   statusChangeCallback(response);
+ });
+
+ };
+
+ // Load the SDK asynchronously
+ (function(d, s, id) {
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) return;
+   js = d.createElement(s); js.id = id;
+   js.src = "//connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
+
+ // Here we run a very simple test of the Graph API after login is
+ // successful.  See statusChangeCallback() for when this call is made.
+ function testAPI() {
+   console.log('Welcome!  Fetching your information.... ');
+   FB.api('/me', function(response) {
+     console.log('Successful login for: ' + response.name);
+     document.getElementById('status').innerHTML =
+       'Thanks for logging in, ' + response.name + '!';
+   });
+ }
+
+ function verifyScope(){
+   FB.api('/me/permissions', function(response) {
+     console.log(response);
+     console.log(scope);
+     var declined = [];
+      for (i = 0; i < response.data.length; i++) {
+        if (response.data[i].status == 'declined') {
+          declined.push(response.data[i].permission)
+        }
+      }
+      if(declined.length > 0){
+        FB.login(function(response) {
+    //
+        }, {
+            scope: declined.join(),auth_type: 'auth_nonce'
+        });
+      }else{
+        return true;
+      }
+   });
+ }
+});
